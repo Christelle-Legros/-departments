2e84da1f74eab005030baab7cd812700ee6461aa
Christelle LEGROS <christelle.legros80@sfr.fr>
2023-12-20 13:38:59 +0100

    Add fichier .env for jokes token

--- src/components/Jokes.jsx
+++ src/components/Jokes.jsx
@@ -8,17 +8,7 @@ const Jokes = () => {
   const [showAnswer, setShowAnswer] = useState(false);
   const [showQuestion, setShowQuestion] = useState(false);
 
-  //   const fetch = require("node-fetch");
-
-  //   fetch("https://www.blagues-api.fr/api/random", {
-  //     headers: {
-  //       Authorization:
-  //         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiODg2OTE1NjczMTgyMzIyNzc5IiwibGltaXQiOjEwMCwia2V5IjoiVFlrT0R4ZEY2Ujl3SXhtV3AwTWRqYng1bEJrMEJJRkU4ek1EaEhzR1E4bkpuQUl6dlkiLCJjcmVhdGVkX2F0IjoiMjAyMy0xMi0xOFQxNDoxMDowOSswMDowMCIsImlhdCI6MTcwMjkwODYwOX0.4ekdc7pHuPbkqshG6qmvahGBIZxTVF2Uv6F0X6rtdqQ",
-  //     },
-  //   })
-  //     .then((res) => res.json())
-  //     // .then((json) => console.log(json));
-  //     .then((json) => setJokeQuestion(json.joke));
+  let cleAPI = process.env.REACT_APP_TOKEN_JOKES;
 
   useEffect(() => {
     axios
@@ -26,8 +16,7 @@ const Jokes = () => {
         "https://www.blagues-api.fr/api/random?disallow=dark&disallow=limit",
         {
           headers: {
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiODg2OTE1NjczMTgyMzIyNzc5IiwibGltaXQiOjEwMCwia2V5IjoiVFlrT0R4ZEY2Ujl3SXhtV3AwTWRqYng1bEJrMEJJRkU4ek1EaEhzR1E4bkpuQUl6dlkiLCJjcmVhdGVkX2F0IjoiMjAyMy0xMi0xOFQxNDoxMDowOSswMDowMCIsImlhdCI6MTcwMjkwODYwOX0.4ekdc7pHuPbkqshG6qmvahGBIZxTVF2Uv6F0X6rtdqQ",
+            Authorization: cleAPI,
           },
         }
       )
